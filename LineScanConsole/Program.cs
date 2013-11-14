121b399f90e37b29d6213dfa72a13b8ad8a995e4
joncash <tw.kchsieh@gmail.com>
2013-11-14 14:07:21 +0800

    (^)line scan 改為事件通知

--- LineScanConsole/Program.cs
+++ LineScanConsole/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace LineScanConsole
 {
@@ -14,8 +15,24 @@ namespace LineScanConsole
 			long w = 4096;
 			long h = 1024;
 			lineScan.SetPEGMode(w, h);
-			//lineScan.Action();
+			lineScan.On_RunningMessage += lineScan_On_RunningMessage;
+			lineScan.On_ErrorOccured += lineScan_On_ErrorOccured;
 			lineScan.StartGrab();
+			while (Console.ReadLine() != "")
+			{
+				Thread.Sleep(200);
+			}
+			//lineScan.StartGrab();
+		}
+
+		static void lineScan_On_ErrorOccured(object sender, GrabImageEventArgs e)
+		{
+			Console.WriteLine(e.Message);
+		}
+
+		static void lineScan_On_RunningMessage(object sender, GrabImageEventArgs e)
+		{
+			Console.WriteLine(e.Message);
 		}
 	}
 }
